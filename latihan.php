cec727b480d7bc556c5119550eb2c51b6546ce0d
Ayudyahaprilia <dyahajaayu@gmail.com>
2025-10-02 12:25:59 +0700

    Create latihan.php

---
+++ latihan.php
@@ -0,0 +1,11 @@
+html>
+    <head>
+        <title>p1</title>
+        <body>
+            <?php
+            $nama = "Ayu Dyah Aprilia";
+            echo "$nama berkata \"katakan namamu!\"";
+            ?>
+            </body> 
+            </head>
+            </html>
